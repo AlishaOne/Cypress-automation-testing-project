2a15f0b0e58027980628c26ad619e3df46e868cc
Alisha <guifengzhou@gmail.com>
2021-12-16 18:17:49 -0800

    adjust code

--- cypress/integration/e2e/login/login.spec.js
+++ cypress/integration/e2e/login/login.spec.js
@@ -6,26 +6,27 @@ describe('Register module', () => {
 
     before(() => {
         cy.log("in before")
-    });
+    })
 
     after(() => {
         cy.log("in after")
-    });
+    })
 
     beforeEach(() => {
         cy.log("in beforeEach")
-    });
+    })
     
     afterEach(() => {
         cy.log("in afterEach")
-    });
+    })
 
-    it('Homepage is correct', () => {
+    it('Login is correct', () => {
         cy.fixture('localData.json').then((localData) => {
         cy.log(localData)
-        cy.visit(localData.localURL)
-        cy.contains(localData.logoText)
+        // cy.get(loginPage.email).type(localData.email)
+        // cy.get(loginPage.password).type(localData.password)
+        // cy.get(loginPage.submit).click()
     })
-    });
+    })
 
 })
