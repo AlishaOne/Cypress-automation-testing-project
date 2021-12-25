96c790f35e9e3bfee057045d0bb18cd500cffb2f
Alisha <guifengzhou@gmail.com>
2021-12-24 23:05:27 -0800

    add tests for new test demo site

--- cypress/integration/pageObjects/pageElements.js
+++ cypress/integration/pageObjects/pageElements.js
@@ -1,7 +1,29 @@
+const loginPageDemoQA = {
+    email: "#userName",
+    password: "#password",
+    submit: "#login",
+    loginPanelText: "Login in Book Store",
+    logout: "#submit"
+}
+
 const loginPage = {
     email: "#txtUsername",
     password: "#txtPassword",
-    submit: ".button"
+    submit: ".button",
+    forgotPass: "//a[text()='Forgot your password?']",
+    loginPanelText: "LOGIN Panel",
+}
+const forgotPassPage = {
+    username: "#securityAuthentication_userName",
+    reset: "#btnSearchValues",
+    cancel: "#btnCancel",
+    forgotPassLabel: "h1",
+    forgotPassText: "Forgot Your Password?",
+    resetCorrectLabel: "#divContent > p",
+    resetCorrectMsg: "Instructions for resetting your password have been sent to",
+    resetRepeatMsg: "There is a password reset request already in the system",
+    resetIncorrectErrorMsg: "Please contact HR admin in order to reset the password",
+
 }
 const headerPage = {
     logout: "//a[text()='Logout']",
@@ -10,6 +32,8 @@ const headerPage = {
 
 
 export default {
+    loginPageDemoQA,
     loginPage,
-    headerPage
+    headerPage,
+    forgotPassPage,
 }
